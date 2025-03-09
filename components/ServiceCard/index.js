2cce619e99466991643da3effa96a0c93cda5e24
saboor ahmed <saboorahmed984@gmail.com>
2025-03-09 19:07:46 +0500

    first commit

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -3,14 +3,15 @@ import { useTheme } from "next-themes";
 
 const ServiceCard = ({ name, description }) => {
   const { theme } = useTheme();
-  const [mounted, setMounted] = useState();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
   return (
     <div
-      className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
+      className={`w-full ml-40 max-w-md sm:max-w-lg md:max-w-xl mx-auto p-4 rounded-lg transition-all ease-out duration-300 ${
         mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
       } hover:scale-105 link`}
     >
@@ -18,7 +19,7 @@ const ServiceCard = ({ name, description }) => {
       <p className="mt-5 opacity-40 text-xl">
         {description
           ? description
-          : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "}
+          : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
       </p>
     </div>
   );
